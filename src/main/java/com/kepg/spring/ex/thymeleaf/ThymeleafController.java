d578439fac6ef2c5499c0fad4f1b3142e15f180b
Kwon_YuSeok <kyus0919@gmail.com>
2025-02-22 23:37:13 +0900

    [권유석] / Spring - 새로 배운 것 : Thymeleaf로 반복문 구성하기 (향상된 for문과 비슷) 1. Thymeleaf 자체로 숫자 반복하기 : th:each="변수:${함수명.sequence(int start, int end)} 2. Controller의 Model을 사용하여 List, Map을 Thymeleaf의 반복문으로 표현 할 수 있다. 3. Map : JAVA => map.getValue("KEY") / Thymeleaf => ${map.KEY} - 이해 안되는 것 : 없음 - 난이도 : 5/10

--- src/main/java/com/kepg/spring/ex/thymeleaf/ThymeleafController.java
+++ src/main/java/com/kepg/spring/ex/thymeleaf/ThymeleafController.java
@@ -1,14 +1,52 @@
 package com.kepg.spring.ex.thymeleaf;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/thymeleaf")
 @Controller
 public class ThymeleafController {
 	
-	@GetMapping("/thymeleaf/ex01")
+	@GetMapping("/ex01")
 	public String ex01() {
 		
 		return "thymeleaf/ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(Model model) {
+		
+		List<String> fruitsList = new ArrayList<>();
+		fruitsList.add("사과");
+		fruitsList.add("바나나");
+		fruitsList.add("딸기");
+		
+		model.addAttribute("fruitsList", fruitsList);
+		
+		// 사용자 정보 관리 리스트
+		List<Map<String,Object>> userList = new ArrayList<>();
+		
+		Map<String, Object> userMap = new HashMap<>();
+		userMap.put("name", "권유석");
+		userMap.put("age", 28);
+		userMap.put("hobby", "독서");
+		userList.add(userMap);
+		
+		userMap = new HashMap<>();
+		userMap.put("name", "김성은");
+		userMap.put("age", 22);
+		userMap.put("hobby", "애니보기");
+		userList.add(userMap);
+		
+		model.addAttribute("userList", userList);
+		
+		return "thymeleaf/ex02";
 	}
 }
